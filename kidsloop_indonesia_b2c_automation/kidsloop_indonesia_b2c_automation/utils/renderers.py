0ae251b60d29025b0737d99e3d57534132639e50
alviandw <alvian@kidsloop.live>
2022-02-08 14:37:48 +0700

    add wib

--- kidsloop_indonesia_b2c_automation/kidsloop_indonesia_b2c_automation/utils/renderers.py
+++ kidsloop_indonesia_b2c_automation/kidsloop_indonesia_b2c_automation/utils/renderers.py
@@ -5,7 +5,7 @@ from django.template import Template, Context
 def render_date_time(date: str, time: str):
     datetime_object = datetime.strptime(f"{date} {time}", "%Y-%m-%d  %H:%M:%S")
     t = Template(
-        '{% load l10n %}{{date_time.date|date:"l, j F Y"|localize  }} jam {{date_time.time|time:"H:i" }}'
+        '{% load l10n %}{{date_time.date|date:"l, j F Y"|localize  }} jam {{date_time.time|time:"H:i" }} WIB'
     )
     c = Context({"date_time": datetime_object})
     return t.render(c)
